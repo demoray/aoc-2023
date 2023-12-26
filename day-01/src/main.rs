67355e442d3726961192c7496ccd3f9daeda733c
Brian Caswell <bcaswell@gmail.com>
2023-12-25 19:02:58 -0500

    make it easy to unit test day 01

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -1,11 +1,18 @@
-use std::io::{stdin, BufRead, BufReader};
+use std::io::{stdin, BufRead, BufReader, Read};
 
 fn main() {
-    let result: u32 = BufReader::new(stdin().lock())
+    let result = run(stdin().lock());
+    println!("{result}");
+}
+
+fn run<R>(handle: R) -> u32
+where
+    R: Read,
+{
+    BufReader::new(handle)
         .lines()
         .filter_map(|l| value(l.ok()?))
-        .sum();
-    println!("{result}");
+        .sum()
 }
 
 fn value(line: String) -> Option<u32> {
@@ -13,4 +20,17 @@ fn value(line: String) -> Option<u32> {
     let first = iter.nth(0)?;
     let last = iter.last().unwrap_or(first);
     Some(first * 10 + last)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::fs::File;
+
+    #[test]
+    fn test_01() -> Result<(), Box<dyn std::error::Error>> {
+        let result = run(File::open("data/example.txt")?);
+        assert_eq!(result, 142);
+        Ok(())
+    }
 }
